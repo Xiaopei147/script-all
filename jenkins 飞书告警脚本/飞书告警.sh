675ec53a88c94f65d1a8068500df541c64c5d2c3
Mr.Pei <95112952+Xiaopei147@users.noreply.github.com>
2023-03-28 09:45:08 +0800

    Update 飞书告警.sh

--- jenkins 飞书告警脚本/飞书告警.sh
+++ jenkins 飞书告警脚本/飞书告警.sh
@@ -2,7 +2,7 @@
 JOB_URL="${JENKINS_URL}job/${JOB_NAME}"
 getBuildState(){
   buildNr=$1
-  curl -u hw:CEGh54lgd8gK6BhR  ${JOB_URL}/${buildNr}/api/json |grep -Po '"result":\s*"\K\w+'
+  curl -u jenkins_user:passwd  ${JOB_URL}/${buildNr}/api/json |grep -Po '"result":\s*"\K\w+'
 }
 
 state=$(getBuildState ${BUILD_NUMBER}  )
@@ -16,9 +16,9 @@ echo ${state}
 if [[ "x${state}" == "xSUCCESS" ]] ; then
    curl -X POST -H "Content-Type: application/json" \
         -d '{"msg_type":"post","content": {"post": {"zh_cn": {"title": "Jenkins构建通知","content": [[{"tag": "text","text": "'"应用名称：$string2\n版本号：$string3\n构建人：$string1\n状态>：成功\n日期：$nowTime"'"}]]} } }}' \
-https://open.feishu.cn/open-apis/bot/v2/hook/e765b8eb-0d60-4969-96cf-734a52547a07
+https://open.feishu.cn/open-apis/bot/v2/hook/e765b8eb-0d60-4969-
 else
    curl -X POST -H "Content-Type: application/json" \
         -d '{"msg_type":"text","content":{"text":"'"Jenkins构建通知\n应用名称：$string2\n版本号：$string3\n构建人：$string1\n状态：失败\n日期：$nowTime"'"}}' \
-https://open.feishu.cn/open-apis/bot/v2/hook/e765b8eb-0d60-4969-96cf-734a52547a07 && exit1
+https://open.feishu.cn/open-apis/bot/v2/hook/e765b8eb-0d60-4969- && exit1
 fi
