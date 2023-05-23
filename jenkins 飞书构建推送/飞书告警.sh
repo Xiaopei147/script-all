019cb935f495bd681c6e59c75185bee901601b2d
Xiaopei147 <pgs_20010317@163.com>
2023-05-23 11:06:23 +0800

    fixbug

--- jenkins 飞书构建推送/飞书告警.sh
+++ jenkins 飞书构建推送/飞书告警.sh
@@ -12,7 +12,7 @@ getBuildUser(){
 
 getcommit(){
   buildNr=$1
-  curl -u hw:CEGh54l  ${JOB_URL}/${buildNr}/api/json  |grep -Po '"msg":\s*"\K..*?(?=")'
+  curl -u hw:CEGh54l  ${JOB_URL}/${buildNr}/api/json  |grep -Po '"msg":\s*"\K..*?(?=")' |tail -1
 }
 
 state=$(getBuildState ${BUILD_NUMBER}  )
@@ -27,9 +27,9 @@ echo ${state}
 
 if [[ "x${state}" == "xSUCCESS" ]] ; then
    curl -X POST -H "Content-Type: application/json" \
-        -d '{"msg_type":"post","content": {"post": {"zh_cn": {"title": "Jenkins构建通知","content": [[{"tag": "text","text": "'"应用名称：$string2\n发起人：$string4\n版本号：$string3\n构建人：$string1\n状态>：成功\n日期：$nowTime\n更改内容：$commit"'"}]]} } }}' \
+        -d '{"msg_type":"post","content": {"post": {"zh_cn": {"title": "Jenkins构建成功","content": [[{"tag": "text","text": "'"应用名称：$string2\n发起人：$string4\n版本号：$string3\n构建人：$string1\n状态>：成功\n日期：$nowTime\n更改内容：$commit"'"}]]} } }}' \
 https://open.feishu.cn/open-apis/bot/v2/hook/65f4093c-c223-4b35-8bcb-
 else
    curl -X POST -H "Content-Type: application/json" \
-        -d '{"msg_type":"text","content":{"text":"'"Jenkins构建通知\n应用名称：$string2\n发起人：$string4\n版本号：$string3\n构建人：$string1\n状态：失败\n日期：$nowTime\n更改内容：$commit"'"}}' \
+        -d '{"msg_type":"text","content":{"text":"'"Jenkins构建失败\n应用名称：$string2\n发起人：$string4\n版本号：$string3\n构建人：$string1\n状态：失败\n日期：$nowTime\n更改内容：$commit"'"}}' \
 https://open.feishu.cn/open-apis/bot/v2/hook/65f4093c-c223-4b35-8bcb- && exit 1
